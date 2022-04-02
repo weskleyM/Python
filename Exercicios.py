20ecb50f9bf913b7bbce458823d12dedea9adde3
weskleyM <weskley512@gmail.com>
2022-04-02 13:44:00 -0300

    Atualizado

--- Exercicios.py
+++ Exercicios.py
@@ -1,6 +1,13 @@
-#Sucessor e Antecessor
+# Sucessor e Antecessor de uma variável
 var = int(input('Digite um número: '))
-sucessor = var+1
-antecessor = var-1
+sucessor = var + 1
+antecessor = var - 1
 print('O antecessor de {} é {}'.format(var, antecessor))
 print('O sucessor de {} é {}'.format(var, sucessor))
+# Dobro, Triplo e Raíz quadrada de uma variável
+dobro = var * 2
+triplo = var * 3
+raizQ = var ** (1 / 2)
+print('O dobro de {} é {}'.format(var, dobro))
+print('O triplo de {} é {}'.format(var, triplo))
+print('A raíz quadrada de {} é {}'.format(var, raizQ))
