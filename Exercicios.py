a219671680ff50f1197dd73591a4949d660c7808
weskleyM <weskley512@gmail.com>
2022-04-02 13:35:22 -0300

    Atualizado

--- Exercicios.py
+++ Exercicios.py
@@ -1,1 +1,6 @@
 #Sucessor e Antecessor
+var = int(input('Digite um número: '))
+sucessor = var+1
+antecessor = var-1
+print('O antecessor de {} é {}'.format(var, antecessor))
+print('O sucessor de {} é {}'.format(var, sucessor))
