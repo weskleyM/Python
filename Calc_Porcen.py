787e9f4d1115f81fa46bcffaef612e0e58931c24
weskleyM <weskley512@gmail.com>
2022-04-02 17:04:11 -0300

    Atualizado

--- Calc_Porcen.py
+++ Calc_Porcen.py
@@ -9,3 +9,11 @@ salario = float(input('\nDigite um valor: R$'))
 x = int(input('Digite o valor do aumento(%): '))
 aumento = salario + (salario * x / 100)
 print('Salário de R${:.2f}, com aumento de {}% fica R${:.2f}'.format(salario, x, aumento))
+
+# Exercicio - Aluguel de carros
+km = float(input('\nDigite KM rodados: '))
+dia = int(input('Digite a qtd de dias alugados: '))
+calcKM = km * 0.15  # R$0,15 por km
+calcD = dia * 60  # R$60,00 por dia
+total = calcD + calcKM
+print('O total a receber por {:.2f}km rodados e {} dias alugados é R${:.2f}'.format(km, dia, total))
