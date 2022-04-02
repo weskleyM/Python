e28069da6f93cecfc6c8434ba02b9b0abe5aecfd
weskleyM <weskley512@gmail.com>
2022-04-02 14:08:52 -0300

    Atualizado

--- Exercicios.py
+++ Exercicios.py
@@ -10,4 +10,9 @@ triplo = var * 3
 raizQ = var ** (1 / 2)  # ou pow(n, (1/2))
 print('O dobro de {} é {}'.format(var, dobro))
 print('O triplo de {} é {}'.format(var, triplo))
-print('A raíz quadrada de {} é {:.2f}'.format(var, raizQ))
+print('A raíz quadrada de {} é {:.2f}\n'.format(var, raizQ))
+# Média aritimética de 2 variáveis
+n1 = float(input('Digite nota 1: '))
+n2 = float(input('Digite nota 2: '))
+media = (n1+n2)/2
+print('A média entre {} e {} é {:.2f}'.format(n1, n2, media))
