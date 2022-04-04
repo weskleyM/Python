238849adb029d998d6018932aef170b99226e41e
weskleyM <weskley512@gmail.com>
2022-04-04 14:14:40 -0300

    Atualizado

--- Condicionais/Triangulo.py
+++ Condicionais/Triangulo.py
@@ -6,6 +6,6 @@ m2 = float(input('Medida 2: '))
 m3 = float(input('Medida 3: '))
 # Para formar um triângulo cada valor deve ser menor que a soma dos outros dois
 if m1 < m2 + m3 and m2 < m1 + m3 and m3 < m1 + m2:
-    print('As três medidas PODEM formar um triângulo!')
+    print('As três medidas \033[4;32mPODEM\033[m formar um triângulo!')
 else:
-    print('As três medidas NÃO PODEM formar um triângulo!')
+    print('As três medidas \033[4;31mNÃO PODEM\033[m formar um triângulo!')
