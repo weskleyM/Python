7430f2b31ae289c8d21f9725a3d0d2d0ceeb808e
weskleyM <weskley512@gmail.com>
2022-04-04 13:24:21 -0300

    Atualizado

---
+++ Condicionais/Maior_Menor.py
@@ -0,0 +1,15 @@
+n1 = int(input('Número 1: '))
+n2 = int(input('Número 2: '))
+n3 = int(input('Número 3: '))
+menor = n1
+maior = n1
+if n2 < n1 and n2 < n3:
+    menor = n2
+if n3 < n1 and n3 < n2:
+    menor = n3
+if n2 > n1 and n2 > n3:
+    maior = n2
+if n3 > n1 and n3 > n2:
+    maior = n3
+print('O menor número é {}'.format(menor))
+print('O maior número é {}'.format(maior))
