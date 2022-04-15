0b540a9fba86df9d611e8d7c5cb2981a1e4b62a5
weskleyM <weskley89@gmail.com>
2022-04-15 16:52:18 -0300

    Atualizado

---
+++ Condicionais/Atleta.py
@@ -0,0 +1,23 @@
+from datetime import date
+
+hoje = date.today().year
+ano = int(input('Ano de nascimento: '))
+idade = hoje - ano
+print('O atleta tem {} anos'.format(idade))
+if 3 <= idade <= 9:
+    c = 'Mirim'    
+    print('Está classificado como {}'.format(c))
+elif idade <= 14:
+    c = 'Infantil'
+    print('Está classificado como {}'.format(c))
+elif idade <= 19:
+    c = 'Junior'
+    print('Está classificado como {}'.format(c))
+elif idade <= 25:
+    c = 'Sênior'
+    print('Está classificado como {}'.format(c))
+elif idade > 25:
+    c = 'Master'
+    print('Está classificado como {}'.format(c))
+else:
+    print('O atleta tem {} anos, ainda não pode competir!'.format(idade))
