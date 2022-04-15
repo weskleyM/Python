eb6a81f10470cbd74be97542913c584e78143bb5
weskleyM <weskley89@gmail.com>
2022-04-15 17:22:51 -0300

    Atualizado

--- Condicionais/Triangulo.py
+++ Condicionais/Triangulo.py
@@ -7,5 +7,7 @@ m3 = float(input('Medida 3: '))
 # Para formar um triângulo cada valor deve ser menor que a soma dos outros dois
 if m1 < m2 + m3 and m2 < m1 + m3 and m3 < m1 + m2:
     print('As três medidas \033[4;32mPODEM\033[m formar um triângulo!')
+    if m1 == m2 == m3:
+        print('É um triângulo equilátero')
 else:
     print('As três medidas \033[4;31mNÃO PODEM\033[m formar um triângulo!')
