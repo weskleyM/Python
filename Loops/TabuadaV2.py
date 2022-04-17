ff870a61a2ac8cd74cda67d7b24304176e99df5f
weskleyM <weskley89@gmail.com>
2022-04-17 15:53:17 -0300

    Atualizado

---
+++ Loops/TabuadaV2.py
@@ -0,0 +1,8 @@
+n = int(input('Escolha um número: '))
+
+print('-='*10)
+print('Tabuada de {}'.format(n))
+print('-='*10)
+print('')
+for i in range(1, 11):
+    print('{} x {:2} = {}'.format(n, i, n*i))
