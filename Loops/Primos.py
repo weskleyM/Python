25017b4d1e3d86d4bfcba4de06eb070bef83e433
weskleyM <weskley89@gmail.com>
2022-04-18 15:49:53 -0300

    Atualizado

--- Loops/Primos.py
+++ Loops/Primos.py
@@ -1,9 +1,13 @@
 cont = 0
 n = int(input('Digite um número: '))
-for i in range(2, n):
+for i in range(1, n+1):
     if n%i==0:
+        print('\033[32m', end='')
         cont+=1
-if cont == 0:
-    print('O número {} é primo!'.format(n))
+    else:
+        print('\033[31m', end='')
+    print('{}'.format(i), end=' ')
+if cont == 2:
+    print('\n\033[mO número {} é primo!'.format(n))
 else:
-    print('O número {} não é primo!'.format(n))
+    print('\n\033[mO número {} não é primo!'.format(n))
