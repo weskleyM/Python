2efb7aa9657e371da8e97a7e09e5e8a47d484f61
weskleyM <weskley89@gmail.com>
2022-04-21 16:02:20 -0300

    Atualizado

--- Loops/while/AdvinhaV2.py
+++ Loops/while/AdvinhaV2.py
@@ -1,10 +1,15 @@
 from random import randint
 
 cont = 1
-pc = randint(1, 5)
+pc = randint(1, 11)
 j = int(input('Digite um número de 1 a 5: '))
 
 while j != pc:
-    j = int(input('Tente novamente: '))
+    if j > pc:
+        print('Quase...um pouco abaixo!')
+        j = int(input('Tente novamente: '))
+    elif j < pc:
+        print('Quase...um pouco acima!')
+        j = int(input('Tente novamente: '))
     cont += 1
 print('Parabéns! Você adivinhou o número em {} tentativas'.format(cont))
