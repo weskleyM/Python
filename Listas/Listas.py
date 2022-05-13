c1f36d25ca88a10f627a20a0579ab0672e6b16d7
weskleyM <weskley512@gmail.com>
2022-05-13 08:47:36 -0300

    Litas em Python atualizado

--- Listas/Listas.py
+++ Listas/Listas.py
@@ -1,12 +1,26 @@
-from operator import index
-
-
-lista = []
-for i in range(5):
-    n = int(input(f'Digite N{i+1}: '))
-    lista.append(n)
-print(lista)
-maior = max(lista)
-menor = min(lista)
-print(f'O maior é: {maior} na posição: {lista.index(maior)+1}')
-print(f'O menor é: {menor} na posição: {lista.index(menor)+1}')
+# # A LISTA RECEBE 5 VALORES E RETORNA O MAIOR E MENOR VALOR COM SUA RESPECTIVA POSIÇÃO
+# lista = []
+# maior = 0
+# menor = 0
+# for i in range(5):
+#     lista.append(int(input('Digite um valor: ')))
+#     if i == 0:
+#         maior = lista[i]
+#         menor = lista[i]
+#     else:
+#         if lista[i] > maior:
+#             maior = lista[i]
+#         if lista[i] < menor:
+#             menor = lista[i]
+# print(lista)
+# # OU PODERIA SER maior = max(lista)
+# # OU PODERIA SER menor = min(lista)
+# print(f'O maior é {maior} nas posições: ', end='')
+# for i, v in enumerate(lista):
+#     if v == maior:
+#         print(f'#{i+1} ', end='')
+# print(f'\nO menor é {menor} nas posições: ', end='')
+# for i, v in enumerate(lista):
+#     if v == menor:
+#         print(f'#{i+1} ', end='')
+# print()
